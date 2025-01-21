6a29154e33fbc94a47775907b7bade5c91c12ea5
Mykhailo Lohachov <lohachov@soramitsu.co.jp>
2025-01-21 10:47:43 +0900

    Time: 102 ms (57.05%) | Memory: 29.8 MB (38.36%) - LeetSync

--- 2145-grid-game/grid-game.py
+++ 2145-grid-game/grid-game.py
@@ -3,6 +3,16 @@ class Solution:
         top_sum = sum(grid[0])
         bot_sum = sum(grid[1])
 
+        # .  .  .  i  .  .
+        # x  x  x  x  a4 a5  | total sum = top_sum
+        # b1 b2 b3 x  x  x   | total sum = bot_sum
+        # 
+        # What the second player can get?
+        # SECOND_SCORE = max [left on top (a(i+1), ..., an), left on bottom (b0, ... b(i-1))]
+        # 
+        # First player wants to minimize this sum:
+        # min across (i=0,n) SECOND_SCORE(i)
+
         res = inf
         acum_top = 0
         acum_bot = bot_sum
