6f93e843213a5875c3ee56781b8520d2c6e5a47b
Mykhailo Lohachov <lohachov@soramitsu.co.jp>
2025-01-22 12:26:04 +0900

    Time: 3803 ms (5.17%) | Memory: 238 MB (20.00%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -0,0 +1,24 @@
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        n = len(isWater)
+        m = len(isWater[0])
+
+        visited = set()
+        cache = []
+        for i in range(n):
+            for j in range(m):
+                if isWater[i][j] == 1:
+                    cache.append((i, j, 0))
+                    isWater[i][j] = 0
+                    visited.add((i,j))
+        
+        while cache:
+            x, y, c = cache.pop(0)
+            # print(x, y, c)
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                if 0 <= x+dx < n and 0 <= y+dy < m and (x+dx,y+dy) not in visited:
+                    visited.add((x+dx,y+dy))
+                    isWater[x+dx][y+dy] = c+1
+                    cache.append((x+dx,y+dy, c+1))
+                    
+        return isWater
