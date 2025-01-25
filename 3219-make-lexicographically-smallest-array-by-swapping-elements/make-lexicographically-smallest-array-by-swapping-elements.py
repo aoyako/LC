908cf43e2923b5cf0f2bbc5b91790ca88470e27f
Mykhailo Lohachov <lohachov@soramitsu.co.jp>
2025-01-25 11:26:46 +0900

    Time: 2199 ms (5.56%) | Memory: 49.1 MB (69.45%) - LeetSync

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/make-lexicographically-smallest-array-by-swapping-elements.py
@@ -1,6 +1,10 @@
 class Solution:
     def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
         cache = sorted(nums)
+
+        # Each cluster is characterized by the lower and upper bound and its elements
+        # Elements in single cluster can be swapped between each other freely
+        # For example, if limit=1, one possible clusters are [1, 2, 3], [5, 6], [8]
         clusters = []
 
         curr_cluster = []
@@ -8,17 +12,17 @@ class Solution:
             if len(curr_cluster) == 0 or x- curr_cluster[-1] <= limit:
                 curr_cluster.append(x)
             else:
+                # (lower_bound, upper_bound, elements)
                 clusters.append(((curr_cluster[0], curr_cluster[-1]), curr_cluster))
                 curr_cluster = [x]
         clusters.append(((curr_cluster[0], curr_cluster[-1]), curr_cluster))
 
         res = []
         for x in nums:
+            # Find element's cluster and replace it with the smallest in the
+            # corresponding cluster
             pos = bisect_right(clusters, x, key=lambda x: x[0][0])
             (lower, upper), cluster = clusters[pos-1]
-            # for (lower, upper), cluster in clusters:
-                # if len(cluster) != 0 and lower <= x and x <= upper:
-            print(lower, upper)
             res.append(cluster[0])
             cluster.pop(0)
 
